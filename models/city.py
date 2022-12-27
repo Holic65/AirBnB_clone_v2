97b77e0279d919a24f3f06a1cfc36595a81096de
holic65 <okormark@gmail.com>
2022-12-26 18:25:43 -0800

    tried editing city and state to run console

--- models/city.py
+++ models/city.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from os import getenv
-import sqlalchemy
+import os
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+
 from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'cities'
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states,id'), nullable=False)
-        places = relationship("Place", backref="cities", cascade="delete")
-
-    else:
-        state_id = ""
-        name = ""
-
-        def __init__(self, *args, **kwargs):
-            '''initalizes city'''
-            super().__init__(*args, **kwargs)
+    __tablename__ = 'cities'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    state_id = Column(
+        String(60), ForeignKey('states.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    places = relationship(
+        'Place',
+        cascade='all, delete, delete-orphan',
+        backref='cities'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
