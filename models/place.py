6f3d07fae7401b23fbffa48b14fd10f65ca2d469
holic65 <okormark@gmail.com>
2022-12-28 16:47:32 -0800

    fixing

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
