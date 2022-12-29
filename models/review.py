0e7e94a2b66c75d2274058262ac028030fc686e1
holic65 <okormark@gmail.com>
2022-12-29 15:49:50 -0800

    reviewsss

--- models/review.py
+++ models/review.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-import os
-from sqlalchemy import Column, ForeignKey, String
-from sqlalchemy.orm import relationship
-
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = 'reviews'
-    place_id = Column(
-        String(60), ForeignKey('places.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    user_id = Column(
-        String(60), ForeignKey('users.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    text = Column(
-        String(1024), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
+
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """intialize review"""
+        super().__init__(*args, **kwargs)
