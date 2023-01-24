36c891eb0fdfb577e9a324b22346eee672248de6
holic65 <okormark@gmail.com>
2023-01-24 03:52:48 -0800

    sql test replaced

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,10 @@
--- a script that prepares a MySQL server for a project
+-- script that prepares a MySQL server for the project
+
+-- create database hbnb_test_db of it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- creates the user hbnb_test and grants all priviledges the user
+-- on hbnb_dev and select privilege on performance_schema
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db. * TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
