bb5aff75bb34eebbbd3a2e5ae61f74299980b156
holic65 <okormark@gmail.com>
2023-01-26 14:57:19 -0800

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,11 +8,11 @@ from os.path import isdir
 def do_pack():
     """ a function that generates .tgz archive """
     try:
-        datename = datetime.now().strftime("%Y%B%D%M%S")
+        datename = datetime.now().strftime("%Y%m%d%H%M%S")
         if not isdir('versions'):
             local('mkdir versions')
         arch_name = 'versions/web_static_{}.tgz'.format(datename)
-        local('tar -zcvf {} web_static'.format(arch_name))
+        local('tar -cvzf {} web_static'.format(arch_name))
         return arch_name
     except:
         return None
