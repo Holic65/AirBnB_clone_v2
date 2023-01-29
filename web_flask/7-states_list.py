4681f76828bab6cd9bd94d7217a86a55d55d3ba5
holic65 <okormark@gmail.com>
2023-01-28 17:19:46 -0800

    task 7 trying

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,54 +1,33 @@
 #!/usr/bin/python3
-"""
-<<<<<<< HEAD
-starts a Flask web application
-"""
-
+'''A simple Flask web application.
+'''
 from flask import Flask, render_template
-from models import *
-from models import storage
-app = Flask(__name__)
 
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-=======
-A simple flask server running on 0.0.0.0:5000
-"""
-from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 
 app = Flask(__name__)
+'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list', strict_slashes=False)
-def display_states():
-    """Prints html document with a list of states"""
-    states = storage.all(State).values()
->>>>>>> d364182adf6b6aebc07fd37d00692d0bd7608f44
-    return render_template('7-states_list.html', states=states)
+@app.route('/states_list')
+def states_list():
+    '''The states_list page.'''
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    ctxt = {
+        'states': all_states
+    }
+    return render_template('7-states_list.html', **ctxt)
 
 
 @app.teardown_appcontext
-<<<<<<< HEAD
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
-=======
-def remove_current_session(exc):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
->>>>>>> d364182adf6b6aebc07fd37d00692d0bd7608f44
