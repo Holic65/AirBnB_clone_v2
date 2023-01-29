aa0e427d0c94b3a03dececf3062adc1633a6d9cd
holic65 <okormark@gmail.com>
2023-01-28 18:33:39 -0800

    task 9 trying

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,34 +14,35 @@ app.url_map.strict_slashes = False
 
 @app.route('/states')
 @app.route('/states/<id>')
-def all_states():
-    '''The cities_by_states page.'''
-    all_states = list(storage.all(State).value())
+def states(id=None):
+    '''The states page.'''
     states = None
     state = None
+    all_states = list(storage.all(State).values())
     case = 404
     if id is not None:
         res = list(filter(lambda x: x.id == id, all_states))
         if len(res) > 0:
             state = res[0]
-            states.cities.sort(key=lambda x: x.name)
+            state.cities.sort(key=lambda x: x.name)
             case = 2
-        else:
+    else:
         states = all_states
         for state in states:
             state.cities.sort(key=lambda x: x.name)
         states.sort(key=lambda x: x.name)
-        case 1
-        ctxt = {
-            'states': states,
-            'state': state,
-            'case': case
-        }
-        return render_template('9-states.html', **ctxt)
+        case = 1
+    ctxt = {
+        'states': states,
+        'state': state,
+        'case': case
+    }
+    return render_template('9-states.html', **ctxt)
 
 
 @app.teardown_appcontext
 def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
 
 
